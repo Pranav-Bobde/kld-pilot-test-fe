257b357ac0b59a3a5ceea03aa1dec60db90e3b39
Pranav Bobde <bobdep31@gmail.com>
2023-01-05 15:33:09 +0530

    add login link

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -6,6 +6,7 @@
       :todos="todos"
       :meta="meta"
     ></example-component>
+    <router-link :to="{ path: '/login' }">LOGIN</router-link>
   </q-page>
 </template>
 
@@ -17,26 +18,26 @@ import { ref } from 'vue';
 const todos = ref<Todo[]>([
   {
     id: 1,
-    content: 'ct1'
+    content: 'ct1',
   },
   {
     id: 2,
-    content: 'ct2'
+    content: 'ct2',
   },
   {
     id: 3,
-    content: 'ct3'
+    content: 'ct3',
   },
   {
     id: 4,
-    content: 'ct4'
+    content: 'ct4',
   },
   {
     id: 5,
-    content: 'ct5'
-  }
+    content: 'ct5',
+  },
 ]);
 const meta = ref<Meta>({
-  totalCount: 1200
+  totalCount: 1200,
 });
 </script>
